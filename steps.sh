1cdc76b926efa31ea00e1b927ae89405ff9a80d4
Lasantha Sanjeewa Silva <41699526+sanju2@users.noreply.github.com>
2023-09-04 21:21:36 +0400

    2023-09-04 commit

---
+++ steps.sh
@@ -0,0 +1,14 @@
+# Flask install locally
+pip install Flask
+
+# Run Application Locally
+python app.py
+
+# Python venv create
+python -m venv venv
+
+# Activate venv
+source venv/bin/activate
+
+# Install Requirements
+pip install -r requirements.txt
